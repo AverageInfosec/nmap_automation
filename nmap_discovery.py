59d375e4f74cf8e9dfcca2a23ff5336d4ceb5e19
AverageInfosec <59807615+AverageInfosec@users.noreply.github.com>
2020-01-23 12:56:52 -0500

    Update nmap_discovery.py

--- nmap_discovery.py
+++ nmap_discovery.py
@@ -9,8 +9,8 @@ import shutil
 
 #Ask for user input and assign IP\subnet and port range.
 print('<' + "-"  * 40 + '>')#style options
-ip_range = str(input('\nEnter an IP or subnet: (Example: 192.168.1.0/24 or 192.168.1.1-50) '))
-user_ports = str(input('\nEnter Ports to scan: (Example: enter just "-" for all ports, 21,22,80,443 or 1-1000)'))
+ip_range = '192.168.3.132' #str(input('\nEnter an IP or subnet: (Example: 192.168.1.0/24 or 192.168.1.1-50) '))
+user_ports = '1-500' #str(input('\nEnter Ports to scan: (Example: enter just "-" for all ports, 21,22,80,443 or 1-1000)'))
 print("\nScanning IP " + ip_range + ". For ports " + user_ports + ".")
 
 def run_nmap():
@@ -19,7 +19,7 @@ def run_nmap():
     #nmap scan config
     nmap_scan = ("nmap " + ip_range + " -p" + user_ports + " " + nmap_arguments + " -oX outfile.xml")
     #convert xml to csv
-    os.system( nmap_scan + '&& python %CD%\\nmap_to_csv.py -f outfile.xml -csv')
+    os.system( nmap_scan )
     os.system('exit' )
     #Finished time
     after = datetime.now()
@@ -63,7 +63,8 @@ while True:
     else:
             print("Please enter a valid option")
 
-    
+os.system("powershell -NoProfile -ExecutionPolicy ByPass .\\xml_to_csv.ps1")
+os.system('exit' )
 
 #Make a copy of scan csv to create an IP list
 shutil.copyfile('scan.csv', 'ip_temp_list.csv')
@@ -83,12 +84,13 @@ writer.writerows(newrows)
 with open("ip_temp_list.csv","r") as source:
     rdr= csv.reader( source )
     next(rdr)
+    next(rdr)
     with open("iplist.csv","w", newline='') as result:
         wtr= csv.writer( result )
-        in_iter= ( (r[0],) for r in rdr )
+        in_iter= ( (r[3],) for r in rdr )
         wtr.writerows( in_iter )
 
-#remove outfile.xml and temp csv file
+#remove temp csv file
 os.remove("ip_temp_list.csv")
 #os.remove("outfile.xml")
 
@@ -101,7 +103,7 @@ for row in rows:
         newrows.append(row)
 writer = csv.writer(open('iplist.csv', 'w', newline=''))
 writer.writerows(newrows)
-
+    
 while True:
     review_report = input('''Would you like to review full report?
     1) Yes
