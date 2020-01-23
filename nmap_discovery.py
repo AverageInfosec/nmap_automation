cb5dbdaf91632b06f420db3a6a095990b039f1b5
AverageInfosec <59807615+AverageInfosec@users.noreply.github.com>
2020-01-23 08:58:30 -0500

    Add files via upload

---
+++ nmap_discovery.py
@@ -0,0 +1,99 @@
+#!c:/Python/python.exe
+import sys
+import os
+import nmap
+from datetime import datetime
+import time
+import csv
+import shutil
+
+
+#Ask for user input and assign IP\subnet and port range.
+print('<' + "-"  * 40 + '>')#style options
+ip_range = '192.168.3.132' #str(input('\nEnter an IP or subnet: (Example: 192.168.1.0/24 or 192.168.1.1-50) '))
+user_ports = '1-500' #str(input('\nEnter Ports to scan: (Example: enter just "-" for all ports or 21,22,80,443 '))
+print("\nScanning IP " + ip_range + ". For ports " + user_ports + ".")
+
+def run_nmap():
+    #add banner containing time stamps for calculating time elapsed
+    before = datetime.now()
+    #nmap scan config
+    nmap_scan = ("nmap " + ip_range + " -p" + user_ports + " " + nmap_arguments + " -oX outfile.xml")
+    #convert xml to csv
+    os.system( nmap_scan + '&& python %CD%\\nmap_to_csv.py -f outfile.xml -csv')
+    os.system('exit' )
+    #Finished time
+    after = datetime.now()
+    print("Time finished: "+ str(datetime.now().strftime('%m-%d-%Y %H:%M:%S')))
+    print('<' + "-"  * 40 + '>')#style options
+    #time elapsed during scan
+    delta =  after-before
+    print("Time elapsed: " + str(delta))
+    print('<' + "-"  * 40 + '>')#style options
+
+scanops = input("""Please select the type of nmap scan you would like to run
+    1) Comprehensive Scan
+    2) SYN ACK Scan
+    3) Scan UDP Ports
+    4) OS Discovery
+    5) Custom \n""")
+print("You have chosen: ", scanops)
+if scanops == '1':
+        nmap_arguments = '-T4 -A -v'
+        run_nmap()
+        print(nmap_arguments)
+elif scanops == '2':
+        nmap_arguments = '-sS -v'
+        run_nmap()
+elif scanops == '3':
+        nmap_arguments = '-sU -v'
+        run_nmap()
+elif scanops == '4':
+        nmap_arguments = '-O'
+        run_nmap()
+elif scanops == '5':
+        nmap_arguments = str(input('\nEnter nmap options: '))
+        run_nmap()
+elif scanops >='6':
+        print("Please enter a valid option")
+
+#Make a copy of scan csv to create an IP list
+shutil.copyfile('scan.csv', 'ip_temp_list.csv')
+
+
+#remove duplicates from scan.csv
+rows = csv.reader(open('scan.csv', 'r', newline=''))
+newrows = []
+for row in rows:
+    if row not in newrows:
+        newrows.append(row)
+writer = csv.writer(open('scan.csv', 'w', newline=''))
+writer.writerows(newrows)
+
+
+#make a csv with just IP's
+with open("ip_temp_list.csv","r") as source:
+    rdr= csv.reader( source )
+    next(rdr)
+    with open("iplist.csv","w", newline='') as result:
+        wtr= csv.writer( result )
+        in_iter= ( (r[0],) for r in rdr )
+        wtr.writerows( in_iter )
+
+#remove outfile.xml and temp csv file
+os.remove("ip_temp_list.csv")
+#os.remove("outfile.xml")
+
+
+#remove duplicates from iplist.csv
+rows = csv.reader(open('iplist.csv', 'r', newline=''))
+newrows = []
+for row in rows:
+    if row not in newrows:
+        newrows.append(row)
+writer = csv.writer(open('iplist.csv', 'w', newline=''))
+writer.writerows(newrows)
+
+
+
+
